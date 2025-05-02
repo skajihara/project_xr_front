80427c9ecf1d303a2d0d4bcae36a0080f3effeb8
skajihara <t15cs015@gmail.com>
2025-05-03 03:35:19 +0900

    dotenv-flowインポート、envファイル読み込み設定追加

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
+import dotenvFlow from 'dotenv-flow'
+
+// __dirname === C:\…\project_xr_front\src
+dotenvFlow.config({
+  path: path.resolve(__dirname, 'env'),  // ← src/env を指す
+})
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ?? '',
+    // 他に公開したい NEXT_PUBLIC_* があればここに追加
+  },
   webpack(config, { dev }) {
     if (dev) {
       // webpack5 のメモリキャッシュに切り替え
